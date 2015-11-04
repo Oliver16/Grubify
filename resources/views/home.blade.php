54fffe74e397e5689b009f6baebcb0595dabce5b
Oliver Curtis <oliver_14@live.com>
2015-11-04 21:26:05 +0000

    Initial Commit
    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    @include('layout.style')
+    <title>Grubify</title>
+    
+    </head>
+    <body>
+        @include('layout.navigation')
+        @include('layout.header')
+        @include('layout.script')
+    </body>
+</html>
